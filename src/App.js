1e542a84735711f2e7cfc74a6b4cb06d170a3193
bartek <bieda@testy.com>
2019-07-22 15:48:00 +0200

    Implemented calculate functionality

--- src/App.js
+++ src/App.js
@@ -1,78 +1,101 @@
 import React from "react";
 import './App.css';
+import * as math from "mathjs";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+  }
+  
+  // Set a value of input
+  addToInput = val => {
+    this.setState({ input: this.state.input + val });
+  };
+  // Change a sign of input's value
+  changeSign = () => {
+    this.setState({ input: this.state.input * (-1) });
+  };
+  // Calculate the result
+  handleEqual = () => {
+    this.setState({ input: math.round(math.evaluate(this.state.input), 3) });
+  };
+
   render() {
     return (    
-      <div class="Calc">
-          <div class="Calc-header">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1242 2208" class="Calc-UI">
-                <g id="Artboard_1" data-name="Artboard 1">
-                    <g>
-                        <rect class="cls-1" width="1242" height="2208"/>
-                        <rect class="cls-2" x="931" y="658" width="311" height="1550"/>
-                        <rect class="cls-3" y="348" width="1242" height="310"/>
-                        <rect id="Rectangle_3_copy" data-name="Rectangle 3 copy" class="cls-4" width="1242" height="348"/>
-                        <text id="inputField" text-anchor="end" class="cls-5" x="1142.826" y="567.032"></text>
-                        <text id="allInputField" text-anchor="end" class="cls-6" x="1132" y="211.031"></text>
-                        <g class="cls-7">
-                            <text id="_1" data-name="1" class="cls-8" x="123.116" y="1159.703">1</text>
-                            <text id="C" class="cls-8" x="114.116" y="849.702">C</text>
-                            <text id="_4" data-name="4" class="cls-8" x="124.116" y="1469.702">4</text>
-                            <text id="_7" data-name="7" class="cls-8" x="128.116" y="1779.703">7</text>
-                            <text id="_0" data-name="0" class="cls-8" x="125.117" y="2089.704">0</text>
-                        </g>
-                        <g class="cls-7">
-                            <text id="_-" data-name="+/-" class="cls-8" x="390.965" y="849.797">+/-</text>
-                            <text id="_2" data-name="2" class="cls-8" x="436.116" y="1159.703">2</text>
-                            <text id="_5" data-name="5" class="cls-8" x="435.116" y="1468.704">5</text>
-                            <text id="_8" data-name="8" class="cls-8" x="437.116" y="1779.703">8</text>
-                            <text id="_" data-name="," class="cls-8" x="457.116" y="2090.702">,</text>
-                        </g>
-                        <g class="cls-7">
-                            <text id="_2-2" data-name="%" class="cls-8" x="732.291" y="849.89">%</text>
-                            <text id="_3" data-name="3" class="cls-8" x="748.116" y="1159.703">3</text>
-                            <text id="_6" data-name="6" class="cls-8" x="745.116" y="1468.704">6</text>
-                            <text id="DEL" class="cls-8" x="697.921" y="2082.157">DEL</text>
-                            <text id="_9" data-name="9" class="cls-8" x="745.116" y="1779.703">9</text>
-                        </g>
-                        <g class="cls-7">
-                            <text id="_3-2" data-name="÷" class="cls-10" x="1054.29" y="863.891">÷</text>
-                            <text id="_4-2" data-name="×" class="cls-10" x="1054.116" y="1174.702">×</text>
-                            <text id="_5-2" data-name="−" class="cls-10" x="1054.116" y="1484.703">−</text>
-                            <text id="_6-2" data-name="=" class="cls-10" x="1053.92" y="2104.156">=</text>
-                            <text id="_7-2" data-name="+" class="cls-10" x="1054.116" y="1793.704">+</text>
-                        </g>
-
-                        {/* input fields */}
-                        <rect class="f1" y="658" width="310" height="310"/>
-                        <rect class="f1" y="968" width="310" height="310"/>
-                        <rect class="f1" y="1278" width="310" height="310"/>
-                        <rect class="f1" y="1588" width="310" height="310"/>
-                        <rect class="f1" y="1898" width="310" height="310"/>
-                    
-                        <rect class="f1" y="658" x="310" width="311" height="310"/>
-                        <rect class="f1" y="968" x="310" width="311" height="310"/>
-                        <rect class="f1" y="1278" x="310" width="311" height="310"/>
-                        <rect class="f1" y="1588" x="310" width="311" height="310"/>
-                        <rect class="f1" y="1898" x="310" width="311" height="310"/>
-                    
-                        <rect class="f1" y="658" x="621" width="310" height="310"/>
-                        <rect class="f1" y="968" x="621" width="310" height="310"/>
-                        <rect class="f1" y="1278" x="621" width="310" height="310"/>
-                        <rect class="f1" y="1588" x="621" width="310" height="310"/>
-                        <rect class="f1" y="1898" x="621" width="310" height="310"/>
-                    
-                        <rect class="f2" y="658" x="931" width="311" height="310"/>
-                        <rect class="f2" y="968" x="931" width="311" height="310"/>
-                        <rect class="f2" y="1278" x="931" width="311" height="310"/>
-                        <rect class="f2" y="1588" x="931" width="311" height="310"/>
-                        <rect class="f2" y="1898" x="931" width="311" height="310"/>
-                    </g>
+      
+      <div className="Calc" onKeyPress={this.handleKeyPress}>
+        <div className="Calc-header">
+          {/* Drawing calc UI */}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1242 2208" className="Calc-UI">
+            <g id="Artboard_1" data-name="Artboard 1">
+              <g>
+                <rect className="cls-1" width="1242" height="2208"/>
+                <rect className="cls-2" x="931" y="658" width="311" height="1550"/>
+                <rect className="cls-3" y="348" width="1242" height="310"/>
+                <rect id="Rectangle_3_copy" data-name="Rectangle 3 copy" className="cls-4" width="1242" height="348"/>
+                <text id="inputField" textAnchor="end" className="cls-5" x="1142.826" y="567.032">{this.state.input}</text>
+                <text id="allInputField" textAnchor="end" className="cls-6" x="1132" y="211.031"></text>
+                <g className="cls-7">
+                  <text id="_1" data-name="1" className="cls-8" x="133.116" y="1159.703">1</text>
+                  <text id="C" className="cls-8" x="114.116" y="849.702">C</text>
+                  <text id="_4" data-name="4" className="cls-8" x="124.116" y="1469.702">4</text>
+                  <text id="_7" data-name="7" className="cls-8" x="128.116" y="1779.703">7</text>
+                  <text id="_0" data-name="0" className="cls-8" x="125.117" y="2089.704">0</text>
                 </g>
-            </svg>
-          </div>
+                <g className="cls-7">
+                  <text id="_-" data-name="+/-" className="cls-8" x="390.965" y="849.797">+/-</text>
+                  <text id="_2" data-name="2" className="cls-8" x="436.116" y="1159.703">2</text>
+                  <text id="_5" data-name="5" className="cls-8" x="435.116" y="1468.704">5</text>
+                  <text id="_8" data-name="8" className="cls-8" x="437.116" y="1779.703">8</text>
+                  <text id="_" data-name="," className="cls-8" x="457.116" y="2090.702">,</text>
+                </g>
+                <g className="cls-7">
+                  <text id="_2-2" data-name="%" className="cls-8" x="732.291" y="849.89">%</text>
+                  <text id="_3" data-name="3" className="cls-8" x="748.116" y="1159.703">3</text>
+                  <text id="_6" data-name="6" className="cls-8" x="745.116" y="1468.704">6</text>
+                  <text id="DEL" className="cls-8" x="697.921" y="2082.157">DEL</text>
+                  <text id="_9" data-name="9" className="cls-8" x="745.116" y="1779.703">9</text>
+                </g>
+                <g className="cls-7">
+                  <text id="_3-2" data-name="÷" className="cls-10" x="1054.29" y="863.891">÷</text>
+                  <text id="_4-2" data-name="×" className="cls-10" x="1054.116" y="1174.702">×</text>
+                  <text id="_5-2" data-name="−" className="cls-10" x="1054.116" y="1484.703">−</text>
+                  <text id="_6-2" data-name="=" className="cls-10" x="1053.92" y="2104.156">=</text>
+                  <text id="_7-2" data-name="+" className="cls-10" x="1054.116" y="1793.704">+</text>
+                </g>
+
+                {/* input fields */}
+                <rect onClick={() => this.setState({input: ""})} className="f1" y="658" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("1")} className="f1" y="968" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("4")} className="f1" y="1278" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("7")} className="f1" y="1588" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("0")} className="f1" y="1898" width="310" height="310"/>
+            
+                <rect onClick={() => this.changeSign()}className="f1" y="658" x="310" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("2")} className="f1" y="968" x="310" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("5")} className="f1" y="1278" x="310" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("8")} className="f1" y="1588" x="310" width="311" height="310"/>
+                <rect onClick={() => this.addToInput(".")} className="f1" y="1898" x="310" width="311" height="310"/>
+            
+                <rect onClick={() => this.addToInput("%")}className="f1" y="658" x="621" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("3")} className="f1" y="968" x="621" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("6")} className="f1" y="1278" x="621" width="310" height="310"/>
+                <rect onClick={() => this.addToInput("9")} className="f1" y="1588" x="621" width="310" height="310"/>
+                <rect onClick={() => this.setState({input: this.state.input.slice(0, -1)})}className="f1" y="1898" x="621" width="310" height="310"/>
+            
+                <rect onClick={() => this.addToInput("/")} className="f2" y="658" x="931" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("*")} className="f2" y="968" x="931" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("-")} className="f2" y="1278" x="931" width="311" height="310"/>
+                <rect onClick={() => this.addToInput("+")} className="f2" y="1588" x="931" width="311" height="310"/>
+                <rect onClick={() => this.handleEqual()} className="f2" y="1898" x="931" width="311" height="310"/>
+              </g>
+            </g>
+          </svg>
         </div>
+      </div>
     );
   }
 }
