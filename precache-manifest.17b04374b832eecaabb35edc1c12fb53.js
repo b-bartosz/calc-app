86f5c3afdb37934e27b8fdf41257d0dd1f9b5ca4
bartek <bieda@testy.com>
2019-07-24 15:07:46 +0200

    Updates

---
+++ precache-manifest.17b04374b832eecaabb35edc1c12fb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420080ca5d7a560861826454dd187f69",
+    "url": "/calc-app/index.html"
+  },
+  {
+    "revision": "99004a38294785e83368",
+    "url": "/calc-app/static/css/main.bf659af1.chunk.css"
+  },
+  {
+    "revision": "a2ff39f6f5c402331b2a",
+    "url": "/calc-app/static/js/2.a6f3fe55.chunk.js"
+  },
+  {
+    "revision": "99004a38294785e83368",
+    "url": "/calc-app/static/js/main.67512e1e.chunk.js"
+  },
+  {
+    "revision": "0414a93caad9c7a15413",
+    "url": "/calc-app/static/js/runtime~main.8c7d8dec.js"
+  },
+  {
+    "revision": "cd408c2d01eb2c9531634b8954b8612c",
+    "url": "/calc-app/static/media/bg.cd408c2d.jpg"
+  }
+]);
