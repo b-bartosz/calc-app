b0155bae220a1b6a771ec7ecb40ccec742835343
b-bartosz <bartosz3646@gmail.com>
2019-07-28 21:06:54 +0200

    Improved sign change and percentage functions. Improved text fitting for input. Implemented support for IE and clearAll function. Fixed bugged keyboard input and disabled dragging on IE.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 import React from "react";
 import './Styles/App.scss';
 import * as math from "mathjs";
@@ -18,23 +20,23 @@ class App extends React.Component {
   
   // Set a value of input
   addToInput = val => {
-    if (this.state.calculateKeyPressed === "yes") {
+    if (this.state.operationKeyPressed === "yes" || this.state.calculateKeyPressed === "yes") {
       if (val === ".") {
         this.setState({ input: "0." });
       } else {
         this.setState({ input: val });   
       }
       this.setState({ calculateKeyPressed: "no" });
+      this.setState({ operationKeyPressed: "no" });
     } else {
-      if (this.state.input.length <= 15) {
+      if (this.state.input.length < 15) {
         if ((this.state.input).includes(".") && val === "."){
           alert("Only one comma allowed.");
         } else {
           if (this.state.input === "0" && val !== ".") {
             this.setState({ input: val });   
-          } else if (this.state.calculateKeyPressed === "yes") {      
-            this.setState({ input: val });
-            this.setState({ calculateKeyPressed: "no" });
+          } else if (this.state.input === "-" && val === ".") {      
+            this.setState({ input: "-0" + val });
           } else {
             this.setState({ input: this.state.input + val });
           }
@@ -56,16 +58,34 @@ class App extends React.Component {
     }
   };
 
+  // Clear all inputs
+  clearAll = () => {
+    this.setState({
+      input: "0",
+      allInput: "",
+      operationKeyPressed: "yes",
+      calculateKeyPressed: "no",
+      percentageKeyPressed: "no",
+      easterMsgCount: 0
+    });
+  };
+
   // Change a sign of input's value
   changeSign = () => {
-    if (this.state.input !== "Infinity") this.setState({ input: (parseFloat(this.state.input) * (-1)).toString() });
+    if (this.state.input === "0") {
+      this.setState({ input: "-" });
+    } else if (this.state.input === "-") {
+      this.setState({ input: "0" });
+    } else if (this.state.input !== "Infinity") {
+      this.setState({ input: (parseFloat(this.state.input) * (-1)).toString() });
+    }
   };
 
   // Add operations to allInput
   mathOperation = (val) => {
-    if (this.state.input !== "Infinity"){
+    if (this.state.input !== "Infinity") {
       if (this.state.operationKeyPressed === "no") {
-        if ((this.state.input).includes("e")){
+        if ((this.state.input).includes("e")) {
           alert("Sorry, number too big :(");
         } else {
           this.setState({ operationKeyPressed: "yes" });
@@ -77,7 +97,7 @@ class App extends React.Component {
             this.setState({ allInput: this.state.allInput + val});
             this.setState({ percentageKeyPressed: "no" });
           }
-          this.setState({ input: "0" });
+          this.setState({ input: "" });
         }
       }
     }
@@ -89,9 +109,14 @@ class App extends React.Component {
       if (this.state.allInput === "") {
         this.setState({ input: "0" });
       } else {
-        let percent = (parseFloat(this.state.allInput.slice(0, -1)) * parseFloat(this.state.input)) / 100;
+        var percent = 0;
+        if (("*/").includes(this.state.allInput.slice(-1))) {
+          percent = parseFloat(this.state.input) / 100;
+        } else {
+          percent = ((math.round(math.evaluate(this.state.allInput.slice(0,-1)), 10)) * parseFloat(this.state.input)) / 100;
+        }
         this.setState({ allInput: this.state.allInput + percent});
-        this.setState({ input: "0" });
+        this.setState({ input: percent.toString() });
         this.setState({ percentageKeyPressed: "yes" });
         this.setState({ operationKeyPressed: "no" });
       }
@@ -101,7 +126,7 @@ class App extends React.Component {
   // Calculate the result
   calcResult = () => {
     this.setState({ easterMsgCount: this.state.easterMsgCount + 1 });
-    if (this.state.easterMsgCount === 4) {
+    if (this.state.easterMsgCount === 9) {
       alert(String.fromCharCode(67,114,101,97,116,101,100,32,98,121,32,66,97,114,116,111,115,122,32,66,105,97,322,97,99,104));
       this.setState({ easterMsgCount: 0 });
     } else {
@@ -126,57 +151,105 @@ class App extends React.Component {
 
   // Display previous input
   displayAllInput = () => {
-    let allInputString = this.state.allInput;
-    let mapObj = {
+    var allInputString = this.state.allInput;
+    var mapObj = {
       "/":"÷",
       "*":"×"
     };
 
-    allInputString = allInputString.replace(/\/|\*/gi, function(matched){
+    allInputString = allInputString.replace(/\/|\*/gi, (matched) => {
       return mapObj[matched];
     });
 
-    if (allInputString.length > 23) {
-      return "..." + allInputString.slice(-23);
-    } else {
-      return allInputString;
-    }
+    if (allInputString.length > 23) return "..." + allInputString.slice(-23);
+    return allInputString;
   }
 
   // Fit text inside input field
-  componentDidUpdate() {
-    let test = document.getElementById("inputField");
-    test.style.fontSize = "160px";
-    let width = test.getBoundingClientRect().width;
+  componentDidUpdate = () => {
+    var inputField = document.getElementById("inputField");
+    var textField = document.getElementById("textField");
+    inputField.style.fontSize = "160px";
+    var width = inputField.getBoundingClientRect().width;
+    var maxWidth = textField.getBoundingClientRect().width;
 
-    while (width > 250) {
-      test.style.fontSize = (parseInt(test.style.fontSize) - 1) + "px";
-      width = test.getBoundingClientRect().width;
+    while (width > maxWidth*0.84) {
+      inputField.style.fontSize = (parseInt(inputField.style.fontSize) - 1) + "px";
+      width = inputField.getBoundingClientRect().width;
     }
   }
 
-  // Input from keyboard
+  // Digits input from keyboard
   handleKeyPress = (event) => {
-    if (("1234567890.").includes(event.key)) {
-      this.addToInput(event.key);
-    } 
-    if (("/*-+").includes(event.key)) {
-      this.mathOperation(event.key);
-    }
-    if (event.keyCode === 13) {
-      this.calcResult();
-    }
-    if (event.keyCode === 8) {
-      this.delInput();
+    if (("1234567890").includes(event.key)) this.addToInput(event.key);
+  };
+
+  // Other input from keyboard
+  handleSpecialKeyPress = (event) => {
+    switch (event.keyCode) {
+      case 13: 
+        this.calcResult();
+        break;
+      case 46: 
+        this.delInput();
+        break;
+      case 67: 
+        this.clearAll();
+        break;
+      case 83: 
+        this.changeSign();
+        break;
+      case 106: 
+        this.mathOperation("*");
+        break;
+      case 107: 
+        this.mathOperation("+");
+        break;
+      case 109: 
+        this.mathOperation("-");
+        break;
+      case 110: 
+        this.addToInput(".");
+        break;
+      case 111: 
+        this.mathOperation("/");
+        break;
+      case 188: 
+        this.addToInput(".");
+        break;
+      default:
+        break;
     }
   };
 
+  // Check if user is using IE browser
+  isIE = () => {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE '); // for IE 10 or older
+    var trident = ua.indexOf('Trident/'); // for IE 11
+
+    return (msie > 0 || trident > 0);
+  };
+
+  // Add event listeners
   componentDidMount = () => {
-    document.addEventListener("keydown", this.handleKeyPress, false);
+    if (!this.isIE()) {
+      document.addEventListener("keydown", this.handleKeyPress, false);
+      document.addEventListener("keydown", this.handleSpecialKeyPress, false);
+    } else {
+      document.ondragstart = () => { return false; };
+      document.addEventListener("keydown", this.handleSpecialKeyPress, false);
+    }
   };
 
+  // Remove event listeners
   componentWillUnmount = () => {
-    document.removeEventListener("keydown", this.handleKeyPress, false);
+    if (!this.isIE()) {
+      document.removeEventListener("keydown", this.handleKeyPress, false);
+      document.removeEventListener("keydown", this.handleSpecialKeyPress, false);
+    } else {
+      document.removeEventListener("keydown", this.handleSpecialKeyPress, false);
+    }
   };
 
   render() {
@@ -184,13 +257,13 @@ class App extends React.Component {
       <div className="Calc" onKeyPress={this.handleKeyPress}>
         <div className="Calc-header">
           {/* Drawing calc UI */}
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1242 2208" className="Calc-UI">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1242 2208" className="Calc-UI" shape-rendering ="crispEdges">
             <g id="Artboard_1" data-name="Artboard 1">
               <g>
                 <rect className="cls-1" width="1242" height="2208"/>
                 <rect className="cls-2" x="931" y="658" width="311" height="1550"/>
-                <rect className="cls-3" y="348" width="1242" height="310"/>
-                <rect id="Rectangle_3_copy" data-name="Rectangle 3 copy" className="cls-4" width="1242" height="348"/>
+                <rect id="textField" className="cls-3" y="348" width="1242" height="310"/>
+                <rect id="allTextField" className="cls-4" width="1242" height="348"/>
                 <text id="inputField" textAnchor="end" className="cls-5 disable-select" x="1142.826" y="567.032">{this.displayInput()}</text>
                 <text id="allInputField" textAnchor="end" className="cls-6 disable-select" x="1132" y="211.031">{this.displayAllInput()}</text>
                 <g className="cls-7">
@@ -222,8 +295,8 @@ class App extends React.Component {
                   <text id="_7-2" data-name="+" className="cls-10" x="1054.116" y="1793.704">+</text>
                 </g>
 
-                {/* input fields */}
-                <rect onClick={() => this.setState({input: "0", allInput: ""})} className="f1" y="658" width="310" height="310"/>
+                {/* Input fields */}
+                <rect onClick={() => this.clearAll()} className="f1" y="658" width="310" height="310"/>
                 <rect onClick={() => this.addToInput("1")} className="f1" y="968" width="310" height="310"/>
                 <rect onClick={() => this.addToInput("4")} className="f1" y="1278" width="310" height="310"/>
                 <rect onClick={() => this.addToInput("7")} className="f1" y="1588" width="310" height="310"/>
